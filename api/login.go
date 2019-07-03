4a621e523941cd81ee4d0a3773ec482ef6921d2f
Tantatorn <timefear007@gmail.com>
2019-07-03 22:37:00 +0700

    add login api

---
+++ api/login.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"ce-boostup-backend/model"
+	"net/http"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+)
+
+//Login authorize and return a cookie
+func Login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	//Throws unauthorized error
+	if !isPasswordCorrect(username, password) {
+		return echo.ErrUnauthorized
+	}
+
+	// Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	//Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "CE 1D"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	//Generate encoded token and send it as response
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
+}
+
+//Accessible accessible without authentication
+func Accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Accessible")
+}
+
+//Restricted cannot access without authentication
+func Restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.String(http.StatusOK, "Welcome "+name+"!")
+}
+
+func isPasswordCorrect(username string, password string) bool {
+	hashedPassword, _ := model.PasswordByUsername(username)
+	err := bcrypt.CompareHashAndPassword([]byte(*hashedPassword), []byte(password))
+	return err == nil
+}
--- route/router.go
+++ route/router.go
@@ -3,6 +3,8 @@ package route
 import (
 	"ce-boostup-backend/api"
 
+	"github.com/labstack/echo/middleware"
+
 	"github.com/labstack/echo"
 )
 
@@ -10,9 +12,17 @@ import (
 func Init() *echo.Echo {
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Home
 	e.GET("/", api.Home)
 
-	//user handlers
+	//login route
+	e.POST("/login", api.Login)
+
+	//user routes
 	e.GET("/users", api.GetAllUsers)
 	e.GET("/users/:id", api.GetUserWithID)
 	e.POST("/users", api.CreateUser)
@@ -20,13 +30,18 @@ func Init() *echo.Echo {
 	e.DELETE("/users", api.DeleteAllUsers)
 	e.DELETE("/users/:id", api.DeleteUserWithSpecificID)
 
-	//problem handlers
+	//problem routes
 	e.GET("/problems", api.GetAllProblems)
 	e.GET("/problems/:id", api.GetProblemWithID)
 	e.POST("/problems", api.CreateProblem)
 	e.PUT("/problems/:id", api.UpdateProblem)
 	e.DELETE("/problems", api.DeleteAllProblems)
 	e.DELETE("problems/:id", api.DeleteProblemWithSpecificID)
+
+	// Restricted group
+	r := e.Group("/restricted")
+	r.Use(middleware.JWT([]byte("secret")))
+	r.GET("", api.Restricted)
 
 	return e
 }
