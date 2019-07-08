c926e4cfbe73587ec615b5eeb526b4ae77dda720
Tantatorn <timefear007@gmail.com>
2019-07-08 23:13:37 +0700

    add create submission request

--- api/submission.go
+++ api/submission.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"ce-boostup-backend/model"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// CreateSubmission create a new submission
+func CreateSubmission(c echo.Context) error {
+	var submission model.Submission
+	if err := c.Bind(&submission); err != nil {
+		return err
+	}
+	model.NewSubmission(submission.UserID, submission.ProblemID, submission.LanguageID, submission.Src)
+	return c.JSON(http.StatusCreated, "created")
+}
--- model/submission.go
+++ model/submission.go
@@ -2,19 +2,23 @@ package model
 
 import (
 	"ce-boostup-backend/db"
+	"ce-boostup-backend/judge0"
+	"fmt"
+	"log"
+	"strconv"
 )
 
 //Submission a model for submission
 type Submission struct {
-	SubmissionID int
-	UserID       int
-	ProblemID    int
-	LanguageID   int
-	Src          string
-	SubmittedAt  string
-	Score        int
-	Runtime      int
-	MemoryUsage  float32
+	SubmissionID int     `json:"submission_id" form:"submission_id"`
+	UserID       int     `json:"user_id" form:"user_id"`
+	ProblemID    int     `json:"problem_id" form:"problem_id"`
+	LanguageID   int     `json:"language_id" form:"language_id"`
+	Src          string  `json:"src" form:"src"`
+	SubmittedAt  string  `json:"submitted_at" form:"submitted_at"`
+	Score        int     `json:"score" form:"score"`
+	Runtime      int     `json:"runtime" form:"runtime"`
+	MemoryUsage  float32 `json:"memory_usage" form:"memory_usage"`
 }
 
 //NewSubmission create a new submission
@@ -24,13 +28,35 @@ func NewSubmission(userID int, problemID int, languageID int, src string) error 
 	runtime := 0.0
 	memory := 0
 
-	//result := judge0.Submit(src, "", "") //empty string is for testcase in the future
-
-	statement := `INSERT INTO submission (usr_id,problem_id,lang_id,src,score,runtime,memory_usage) VALUES ($1,$2,$3,$4,$5,$6)`
-	_, err := db.DB.Exec(statement, userID, problemID, languageID, src, score, runtime, memory)
+	testcase, err := SpecificTestcaseWithID(userID)
 	if err != nil {
 		return err
 	}
+
+	for i := range testcase {
+		result := judge0.Submit(src, testcase[i].Input, testcase[i].Output) //empty string is for testcase in the future
+		memory += result.Memory
+		runtime += stringToFloat(result.Time)
+	}
+
+	length := len(testcase)
+	runtime = runtime / float64(length)
+	memory = memory / length
+
+	statement := `INSERT INTO submission (usr_id,problem_id,lang_id,src,score,runtime,memory_usage) VALUES ($1,$2,$3,$4,$5,$6,$7)`
+	_, err = db.DB.Exec(statement, userID, problemID, languageID, src, score, runtime, memory)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	return nil
-	//wait for connecting to Judge0 api
+}
+
+func stringToFloat(str string) float64 {
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		log.Fatal(err)
+		return 0.0
+	}
+	return value
 }
