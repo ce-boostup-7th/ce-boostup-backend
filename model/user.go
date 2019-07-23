2ec15b75aabc6574f1549d8ef1c5d8df6c297545
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-07-23 14:03:55 +0700

    Change api not return user password

--- model/user.go
+++ model/user.go
@@ -13,6 +13,13 @@ type User struct {
 	Score    float64 `json:"score" form:"score"`
 }
 
+// UserRes a grader user model
+type UserRes struct {
+	ID       int     `json:"id" form:"id"`
+	Username string  `json:"username" form:"username"`
+	Score    float64 `json:"score" form:"score"`
+}
+
 //NewUser add a new user
 func NewUser(username string, password string) error {
 	statement := `INSERT INTO grader_user (username,password) VALUES ($1,$2)`
@@ -24,18 +31,18 @@ func NewUser(username string, password string) error {
 }
 
 //AllUsers Get all users info from db
-func AllUsers() ([]*User, error) {
-	rows, err := db.DB.Query("SELECT * FROM grader_user ORDER BY id")
+func AllUsers() ([]*UserRes, error) {
+	rows, err := db.DB.Query("SELECT public.grader_user.id, public.grader_user.username, public.grader_user.score FROM public.grader_user ORDER BY id")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	users := make([]*User, 0)
+	users := make([]*UserRes, 0)
 	for rows.Next() {
-		user := new(User)
+		user := new(UserRes)
 
-		err := rows.Scan(&user.ID, &user.Username, &user.Password, &user.Score)
+		err := rows.Scan(&user.ID, &user.Username, &user.Score)
 		if err != nil {
 			return nil, err
 		}
@@ -50,13 +57,13 @@ func AllUsers() ([]*User, error) {
 }
 
 //SpecificUserWithID return specific user from db by using id
-func SpecificUserWithID(id int) (*User, error) {
-	statement := `SELECT * FROM grader_user WHERE id=$1`
+func SpecificUserWithID(id int) (*UserRes, error) {
+	statement := `SELECT public.grader_user.id, public.grader_user.username, public.grader_user.score FROM public.grader_user WHERE id=$1`
 	row := db.DB.QueryRow(statement, id)
 
-	user := new(User)
+	user := new(UserRes)
 
-	err := row.Scan(&user.ID, &user.Username, &user.Password, &user.Score)
+	err := row.Scan(&user.ID, &user.Username, &user.Score)
 	if err != nil {
 		return nil, err
 	}
